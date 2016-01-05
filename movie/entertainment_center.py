4181879a5a95fde085afdb1a7d47d75a50f9c71f
George <geoluther@users.noreply.github.com>
2016-01-05 11:25:01 -0700

    add comments

--- movie/entertainment_center.py
+++ movie/entertainment_center.py
@@ -5,18 +5,18 @@ import media
 # instantiate movie objects
 hateful_eight = media.Movie("The Hateful Eight", 
 	"https://youtu.be/nIOmotayDMY",
-	"https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/The_Hateful_Eight.jpg/220px-The_Hateful_Eight.jpg") #noqa
+	"https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/The_Hateful_Eight.jpg/220px-The_Hateful_Eight.jpg") # noqa
 
 
 rock_the_kasbah =  media.Movie("Rock The Kasbah", 
 	"https://www.youtube.com/watch?v=jtvcb5N_kek",
-	"https://upload.wikimedia.org/wikipedia/en/thumb/7/78/Rock_the_Kasbah.jpg/220px-Rock_the_Kasbah.jpg") #noqa
+	"https://upload.wikimedia.org/wikipedia/en/thumb/7/78/Rock_the_Kasbah.jpg/220px-Rock_the_Kasbah.jpg") # noqa
 
 last_witchhunter = media.Movie("The Last Witch Hunter", 
 	"https://www.youtube.com/watch?v=xsuG2JUgs_8",
-	"https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Last_Witch_Hunter_poster.jpg/220px-The_Last_Witch_Hunter_poster.jpg") #noqa
+	"https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/The_Last_Witch_Hunter_poster.jpg/220px-The_Last_Witch_Hunter_poster.jpg") # noqa
 
-# build list of movie list
+# build list of movie objects
 movies = [ hateful_eight, rock_the_kasbah, last_witchhunter]
 
 # build the webpage with movie previews
