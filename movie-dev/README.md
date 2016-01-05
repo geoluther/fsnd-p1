0bba65b3f8045c44dbf603a9f43412426ce4f6e0
George <my latest commit>
2016-01-04 18:24:51 -0700

    renames

---
+++ movie-dev/README.md
@@ -0,0 +1,6 @@
+
+# Movie Trailer Webpage
+
+To Run:
+python entertainment_center.py
+will open in a web browser
---
+++ movie-dev/media.py
@@ -0,0 +1,12 @@
+
+import webbrowser
+
+
+class Movie():
+	"""A Movie class that contains the movie's title,
+	preview url, and box art url"""
+
+	def __init__(self, title, preview_url, box_art_url):
+		self.title = title
+		self.trailer_youtube_url = preview_url
+		self.poster_image_url = box_art_url
